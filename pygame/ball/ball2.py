3a66eb158aa01ec418af78aa464b1ea674c2d58b
Tung Duong <tung.duong.thanh@ntq-solution.com.vn>
2018-05-18 17:25:13 +0700

    Refractor

--- pygame/ball/ball2.py
+++ pygame/ball/ball2.py
@@ -5,11 +5,11 @@ RED = 255, 0, 0
 
 screenX, screenY = 640, 480
 screen = pygame.display.set_mode((screenX, screenY))
+r = 40
 
 def drawBall(x, y, r, color):
     pygame.draw.ellipse(screen, color, pygame.Rect(x-r, y-r, 2*r, 2*r))
 
-r = 40
 screen.fill(WHITE)
 drawBall(screenX/2, screenY/2, r, RED)
 pygame.display.flip()
--- pygame/ball/ball5.py
+++ pygame/ball/ball5.py
@@ -6,14 +6,14 @@ RED = 255, 0, 0
 screenX, screenY = 640, 480
 screen = pygame.display.set_mode((screenX, screenY))
 
-def drawBall(x, y, r, color):
-    pygame.draw.ellipse(screen, color, pygame.Rect(x-r, y-r, 2*r, 2*r))
-
 r = 40
 x = screenX/2
 y = r
 vy = 0
 a = 0.05
+
+def drawBall(x, y, r, color):
+    pygame.draw.ellipse(screen, color, pygame.Rect(x-r, y-r, 2*r, 2*r))
 
 clock = pygame.time.Clock()
 while True:
