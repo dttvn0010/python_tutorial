eef9711e41058f582ec38a86420561eeda8865f3
Tung Duong <tung.duong.thanh@ntq-solution.com.vn>
2018-05-18 16:52:11 +0700

    Replace tab in guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -4,13 +4,13 @@ high = 1000
 print('Bạn hãy nghĩ trong đầu một số trong phạm vi từ 0 đến 1000, sau đó trả lời các câu hỏi sau.')
 
 for i in range(10):
-	mid = (low + high) // 2
-	a = input('Số đó lớn hơn ' + str(mid)  + ' ? (Y/N) : ')
-	
-	if a == 'Y':
-		low = mid
-	else:
-		high = mid
+    mid = (low + high) // 2
+    a = input('Số đó lớn hơn ' + str(mid)  + ' ? (Y/N) : ')
+    
+    if a == 'Y':
+        low = mid
+    else:
+        high = mid
 
 print('Số bạn nghĩ là ', high)
 
