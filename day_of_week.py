06caba063ad1e3cde147da1115a609aacabdf865
Tung Duong <tung.duong.thanh@ntq-solution.com.vn>
2018-05-21 09:13:48 +0700

    Refractor

--- day_of_week.py
+++ day_of_week.py
@@ -2,8 +2,8 @@ day_of_week = ['Thứ hai', 'Thứ ba', 'Thứ tư', 'Thứ năm', 'Thứ sáu',
 days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def get_day_of_week(date, month, year):
-    year -= 2000    
-    days_since_2000 = (year//4) * (365*4+1) + 5
+    year -= 2000
+    days_since_2000 = (year//4) * (365*4+1)
     days_since_2000 += (year%4) * 365
    
     for x in range(month-1):
@@ -14,7 +14,7 @@ def get_day_of_week(date, month, year):
     if year%4 != 0 or month > 2:
         days_since_2000 += 1
 
-    return day_of_week[days_since_2000%7]
+    return day_of_week[(5+days_since_2000) % 7]
 
 print(get_day_of_week(21, 5, 2018))
     
