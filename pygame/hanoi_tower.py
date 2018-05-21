38f57544974eb92ad4b5771849c262f011aaaa13
Tung Duong <tung.duong.thanh@ntq-solution.com.vn>
2018-05-21 14:02:04 +0700

    Refractor

--- pygame/hanoi_tower.py
+++ pygame/hanoi_tower.py
@@ -15,63 +15,57 @@ disks = [[], [], []]
 screen = pygame.display.set_mode((screenX, screenY))
 screen.fill(WHITE)
 
+def getRect(disk):
+    xcenter = (disk['column'] + 0.5) * column_width
+    left = xcenter - disk['width']/2
+    top = screenY - disk_height * disk['level']
+    return pygame.Rect(left, top, disk['width'], disk_height-1)
+
 def pushDisk(disk, column):
-    disk.column = column
-    disk.level = len(disks[column]) + 1
-    disk.calcRect()
+    disk['column'] = column
+    disk['level'] = len(disks[column]) + 1
     disks[column].append(disk)
 
 def popDisk(column):
     disks[column].pop()
 
-class Disk():
-    def __init__(self, width):
-        self.width = width
-        self.column = 0
-        self.level = 0    
+def animateMove(old_rect, new_rect):
+    pygame.draw.rect(screen, WHITE, old_rect)
+    pygame.draw.rect(screen, GREEN, new_rect)
+    pygame.display.flip()
+    time.sleep(0.5)
 
-    def calcRect(self):
-        xcenter = (self.column + 0.5) * column_width
-        left = xcenter - self.width/2
-        top = screenY - disk_height * self.level
-        self.rect = pygame.Rect(left, top, self.width, disk_height-1)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            sys.exit()
 
-    def animateMove(self, old_rect, new_rect):
-        pygame.draw.rect(screen, WHITE, old_rect)
-        pygame.draw.rect(screen, GREEN, new_rect)
-        pygame.display.flip()
-        time.sleep(0.5)
+def moveDisk(disk, column):        
+    popDisk(disk['column'])
+    
+    rect1 = getRect(disk)
+    rect2 = pygame.Rect(rect1)
+    rect2.top = 0
 
-    def moveToColumn(self, column):        
-        popDisk(self.column)
-        
-        rect1 = self.rect
-        rect2 = pygame.Rect(rect1)
-        rect2.top = 0        
+    pushDisk(disk, column)
+    rect4 = getRect(disk)
+    rect3 = pygame.Rect(rect4)
+    rect3.top = 0
 
-        pushDisk(self, column)
-        rect3 = pygame.Rect(self.rect)
-        rect3.top = 0
+    animateMove(rect1, rect2)
+    animateMove(rect2, rect3)
+    animateMove(rect3, rect4)
 
-        self.animateMove(rect1, rect2)
-        self.animateMove(rect2, rect3)
-        self.animateMove(rect3, self.rect)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: 
-                sys.exit()
-
-def moveDisk(src, dest, ndisks):
+def hanoi(src, dest, ndisks):
     if ndisks > 0:
-        moveDisk(src, 3-src-dest, ndisks-1)
-        disks[src][-1].moveToColumn(dest)
-        moveDisk(3-src-dest, dest, ndisks-1)
+        hanoi(src, 3-src-dest, ndisks-1)
+        moveDisk(disks[src][-1], dest)
+        hanoi(3-src-dest, dest, ndisks-1)
 
 for i in range(ndisks):
     disk_width = column_width * (ndisks - i) / ndisks
-    disk = Disk(disk_width)
+    disk = {'width' : disk_width}
     pushDisk(disk, 0)
-    pygame.draw.rect(screen, GREEN, disk.rect)
+    pygame.draw.rect(screen, GREEN, getRect(disk))
 
 pygame.display.flip()
-moveDisk(0, 2, ndisks)
+hanoi(0, 2, ndisks)
--- pygame/hanoi_tower_manual.py
+++ pygame/hanoi_tower_manual.py
@@ -13,37 +13,30 @@ disks = [[], [], []]
 
 screen = pygame.display.set_mode((screenX, screenY))
 
+def getRect(disk):
+    xcenter = (disk['column'] + 0.5) * column_width
+    left = xcenter - disk['width']/2
+    top = screenY - disk_height * disk['level']
+    return pygame.Rect(left, top, disk['width'], disk_height-1)
+
 def canPush(disk, column):    
     if len(disks[column]) == 0:
         return True
 
     top_disk = disks[column][-1]
-    return top_disk.width > disk.width
+    return top_disk['width'] > disk['width']
 
 def pushDisk(disk, column):
-    disk.column = column
-    disk.level = len(disks[column]) + 1
-    disk.calcRect()
+    disk['column'] = column
+    disk['level'] = len(disks[column]) + 1
     disks[column].append(disk)
 
 def popDisk(column):
     disks[column].pop()
 
-class Disk():
-    def __init__(self, width):
-        self.width = width
-        self.level = 0
-        self.column = 0
-
-    def calcRect(self):
-        xcenter = (self.column + 0.5) * column_width
-        left = xcenter - self.width/2
-        top = screenY - disk_height * self.level
-        self.rect = pygame.Rect(left, top, self.width, disk_height-1)
-
 for i in range(ndisks):
     disk_width = column_width * (ndisks - i) / ndisks
-    disk = Disk(disk_width)
+    disk = {'width' : disk_width}
     pushDisk(disk, 0)
 
 move_disk = None
@@ -53,7 +46,11 @@ while True:
 
     for i in range(3):
         for disk in disks[i]:
-            pygame.draw.rect(screen, GREEN, disk.rect)
+            if disk != move_disk:
+                pygame.draw.rect(screen, GREEN, getRect(disk))
+
+    if move_disk != None:
+        pygame.draw.rect(screen, GREEN, move_rect)
         
     pygame.display.flip()
 
@@ -73,21 +70,22 @@ while True:
                     continue
 
                 top_disk = disks[i][-1]
-                if top_disk.rect.collidepoint(mouse_pos):
+                top_disk_rect = getRect(top_disk)
+                
+                if top_disk_rect.collidepoint(mouse_pos):
                     move_disk = top_disk
+                    move_rect = getRect(move_disk)
                     break                    
 
         if event.type == pygame.MOUSEMOTION and move_disk != None:
-            move_disk.rect.topleft = mouse_pos            
+            move_rect.topleft = mouse_pos            
 
         if event.type == pygame.MOUSEBUTTONUP and move_disk != None:
             column = int(mouse_pos[0]/column_width)
 
             if canPush(move_disk, column):
-                popDisk(move_disk.column)
+                popDisk(move_disk['column'])
                 pushDisk(move_disk, column)
-            else:
-                move_disk.calcRect()
 
             move_disk = None
 
