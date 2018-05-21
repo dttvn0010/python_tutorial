82bf00d5a8c3e1b06ae82843b74aedbd6b1e19ec
Tung Duong <tung.duong.thanh@ntq-solution.com.vn>
2018-05-21 09:53:20 +0700

    Refractor

--- word_count.py
+++ word_count.py
@@ -1,15 +1,14 @@
 text = 'một năm có mười hai tháng , tháng hai có hai mươi tám ngày , các tháng còn lại có ba mươi hoặc ba mươi mốt ngày'
 
-words_count = {}
-
+text = text.lower()
 for c in ['.', ',' , ':']:
     text = text.replace(c, ' ')
 
+word_counts = {}
 for word in text.split():
     words_count[word] = words_count.get(word, 0) + 1
 
 result = []
-
 for (word, count) in words_count.items():
     result.append((count, word))
 
