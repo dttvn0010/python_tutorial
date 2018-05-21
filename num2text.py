c971ec7c077e9e8cf8cf24292a0a79177276b971
Tung Duong <tung.duong.thanh@ntq-solution.com.vn>
2018-05-21 08:56:47 +0700

    Refractor

--- num2text.py
+++ num2text.py
@@ -1,43 +1,41 @@
-chuso = ['không', 'một', 'hai', 'ba', 'bốn', 'năm', 'sáu', 'bảy', 'tám', 'chín', 'mười']
+chuso = ['không', 'một', 'hai', 'ba', 'bốn', 'năm', 'sáu', 'bảy', 'tám', 'chín']
 
 def convert2digit(x):
+    if x < 10:
+        return chuso[x]
+
     chuc = x // 10
     donvi = x % 10
     
-    text = ''
-    
-    if chuc == 1:
-        text = chuso[10]
-    else:
-        text = chuso[chuc] + ' mươi'
+    text = (chuso[chuc] + ' mươi') if chuc > 1 else 'mười'    
 
     if donvi > 0:
         text += ' '
 
         if donvi == 5:
             text += 'lăm'
+
         elif donvi == 1 and chuc > 1:
             text += 'mốt'
+
         else:
             text += chuso[donvi]
 
     return text
 
-def convert(x):
-    if x <= 10:
-        return chuso[x]
+def convert3digit(x):
+    if x < 100:
+        return convert2digit(x)
 
     tram = x // 100
     chuc = (x//10) % 10
     donvi = x % 10
  
-    if tram == 0:
-        return convert2digit(x)
-  
     text = chuso[tram] + ' trăm'
 
     if chuc > 0:
         text += ' ' + convert2digit(x%100)
+
     elif donvi > 0:
         text += ' lẻ ' + chuso[donvi]
 
