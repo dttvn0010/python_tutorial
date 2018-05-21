df683c85781664e91a7d433c9ccaef099f630041
Tung Duong <tung.duong.thanh@ntq-solution.com.vn>
2018-05-21 08:58:42 +0700

    Refractor

--- text2num.py
+++ text2num.py
@@ -3,10 +3,10 @@ chuso = {'không' : 0, 'một' : 1, 'hai' : 2, 'ba' : 3, 'bốn' : 4, 'năm' : 5
 def text2num(text):
     num = 0
 
-    for word in text.split():
+    for word in text.lower().split():
         if word in chuso:
             num = 10*num + chuso[word]
 
     return num
 
-print(text2num("một một năm"))
+print(text2num("Một một năm"))
