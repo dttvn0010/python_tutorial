73cabbb4622c7e7ed1046f66cfbb3a0057b978bd
Tung Duong <tung.duong.thanh@ntq-solution.com.vn>
2018-05-22 11:13:46 +0700

    Refractor

--- pygame/flappy_bird.py
+++ pygame/flappy_bird.py
@@ -38,9 +38,9 @@ while True:
         if event.type == pygame.KEYUP: 
             a = 0.05        
 
-    for pipe in pipes:
-        check_rect = pygame.Rect(x+2, y+2, bird_size-4, bird_size-4)
+    check_rect = pygame.Rect(x+2, y+2, bird_size-4, bird_size-4)
 
+    for pipe in pipes:
         if check_rect.colliderect(pipe):
             text = font.render('Game Over!', False, RED)
             screen.blit(text, (280, 220))
@@ -71,11 +71,11 @@ while True:
         for pipe in pipes:
             pipe.left -= 1
 
+        pipes = [pipe for pipe in pipes if pipe.left >= 0]
+
         frameNo += 1
         if frameNo % 150 == 0:
             height = random.randint(25, 250)
             lowerTop = height + random.randint(75, 250)
             pipes.append(pygame.Rect(screenX, 0, 10, height))
             pipes.append(pygame.Rect(screenX, lowerTop, 10, screenY-lowerTop))
-
-        pipes = [pipe for pipe in pipes if pipe.left >= 0]
