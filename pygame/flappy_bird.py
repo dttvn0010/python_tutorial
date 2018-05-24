60bcf9cd1f4eb2687175e81b272950984dbb418b
Tung Duong <tung.duong.thanh@ntq-solution.com.vn>
2018-05-24 08:52:37 +0700

    Refractor

--- pygame/flappy_bird.py
+++ pygame/flappy_bird.py
@@ -32,26 +32,14 @@ while True:
             a = -0.2            
 
         if event.type == pygame.KEYUP: 
-            a = 0.05        
-
-    check_rect = pygame.Rect(x+2, y+2, bird_size-4, bird_size-4)
-
-    for pipe in pipes:
-        if check_rect.colliderect(pipe):
-            text = font.render('Game Over!', False, (255, 0, 0))
-            screen.blit(text, (280, 220))
-            pygame.display.flip()
-            finished = True
-            break
-
+            a = 0.05
+    
     if not finished:
         screen.fill((255, 255, 255))
         screen.blit(image, pygame.Rect(x, y, bird_size, bird_size))
 
         for pipe in pipes:
             pygame.draw.rect(screen, (0, 255, 0), pipe)
-
-        pygame.display.flip()
 
         vy += a
         y += vy
@@ -64,8 +52,16 @@ while True:
             y = 0
             vy = 0.0
 
+        check_rect = pygame.Rect(x+2, y+2, bird_size-4, bird_size-4)
+
         for pipe in pipes:
             pipe.left -= 1
+
+            if check_rect.colliderect(pipe):
+                text = font.render('Game Over!', False, (255, 0, 0))
+                screen.blit(text, (280, 220))
+                finished = True
+                break
 
         pipes = [pipe for pipe in pipes if pipe.left >= 0]
 
@@ -75,3 +71,5 @@ while True:
             lowerTop = height + random.randint(75, 250)
             pipes.append(pygame.Rect(screenX, 0, 10, height))
             pipes.append(pygame.Rect(screenX, lowerTop, 10, screenY-lowerTop))
+
+        pygame.display.flip()
