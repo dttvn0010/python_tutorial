405e80fba82764b55073f0ce4ad6e0170a406b4e
Tung Duong <tung.duong.thanh@ntq-solution.com.vn>
2018-05-24 09:35:45 +0700

    Refractor

--- pygame/flappy_bird.py
+++ pygame/flappy_bird.py
@@ -1,4 +1,4 @@
-import sys, pygame, random
+import sys, pygame, time, random
 
 screenSize = screenX, screenY = 640, 480
 screen = pygame.display.set_mode(screenSize)
@@ -16,8 +16,6 @@ image = pygame.transform.scale(image, (bird_size, bird_size))
 
 pipes = []
 frameNo = 0
-finished = False
-
 clock = pygame.time.Clock()
 
 while True:
@@ -31,41 +29,45 @@ while True:
             a = -0.2            
 
         if event.type == pygame.KEYUP: 
-            a = 0.05
+            a = 0.05        
+
+    vy += a
+    y += vy
+
+    if y + bird_size > screenY:
+        vy, y = 0, screenY - bird_size
+
+    if y < 0:
+        vy, y = 0, 0
+
+    screen.fill((255, 255, 255))
+    screen.blit(image, pygame.Rect(x, y, bird_size, bird_size))
     
-    if not finished:
-        screen.fill((255, 255, 255))        
+    bird_rect = pygame.Rect(x+2, y+2, bird_size-4, bird_size-4)
 
-        vy += a
-        y += vy
+    game_over = False
 
-        if y + bird_size > screenY:
-            vy, y = 0, screenY - bird_size
+    for pipe in pipes:
+        pipe.left -= 1
+        pygame.draw.rect(screen, (0, 255, 0), pipe)            
 
-        if y < 0:
-            vy, y = 0, 0
+        if bird_rect.colliderect(pipe):
+            game_over = True
 
-        screen.blit(image, pygame.Rect(x, y, bird_size, bird_size))
+    pipes = [pipe for pipe in pipes if pipe.left >= 0]
 
-        bird_rect = pygame.Rect(x+2, y+2, bird_size-4, bird_size-4)
+    frameNo += 1
+    if frameNo % 150 == 0:
+        h1 = random.randint(25, 250)
+        h2 = h1 + random.randint(75, 250)
+        pipes.append(pygame.Rect(screenX, 0, 10, h1))
+        pipes.append(pygame.Rect(screenX, h2, 10, screenY-h2))
 
-        for pipe in pipes:
-            pipe.left -= 1
-            pygame.draw.rect(screen, (0, 255, 0), pipe)            
+    pygame.display.flip()
 
-            if bird_rect.colliderect(pipe):
-                text = font.render('Game Over!', False, (255, 0, 0))
-                screen.blit(text, (280, 220))
-                finished = True
-                break
-
-        pipes = [pipe for pipe in pipes if pipe.left >= 0]
-
-        frameNo += 1
-        if frameNo % 150 == 0:
-            h1 = random.randint(25, 250)
-            h2 = h1 + random.randint(75, 250)
-            pipes.append(pygame.Rect(screenX, 0, 10, h1))
-            pipes.append(pygame.Rect(screenX, h2, 10, screenY-h2))
-
+    if game_over:
+        text = font.render('Game Over!', False, (255, 0, 0))
+        screen.blit(text, (280, 220))
         pygame.display.flip()
+        time.sleep(1)
+        sys.exit()
